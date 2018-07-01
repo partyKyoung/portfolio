49c88ddd8b20a279d885c7ca7f2c0fe12e33950b
Youkyoung Park <kyoungah@kyoungah.com>
2018-07-01 23:56:52 +0900

    my career 퍼블리싱

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ require("../css/common.scss");
 require("../css/header.scss");
 require("../css/aboutMe.scss");
 require("../css/mySkill.scss");
+require("../css/myCareer.scss");
 
 $(document).ready(function() {
   $(".portfolio").fullpage({
