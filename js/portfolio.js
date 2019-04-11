afddc9ca9d6d8b903ca1008dc98c5f9e01b70194
Youkyoung Park <youkyoung.park@connexus.shop>
2019-04-12 01:34:28 +0900

    포트폴리오 css 수정 중

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,20 +1,14 @@
 require("../css/common.scss");
+require("../css/loading.scss");
 require("../css/header.scss");
 require("../css/aboutMe.scss");
 require("../css/mySkill.scss");
-require("../css/myCareer.scss");
-require("jquery");
+// require("../css/myCareer.scss");
 
-var $ = require("jquery");
-var fullpage = require("fullpage.js");
+setTimeout(() => {
+  const loading = document.getElementById("loading");
+  const portfolio = document.getElementById("portfolio");
 
-$(document).ready(function() {
-  var portfolio = new fullpage("#portfolio", {
-    licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-    css3: true,
-    easing: "easeInOutCubic",
-    easingcss3: "ease",
-    menu: "#portfolio-nav-menu",
-    anchors: ["aboutMe", "mySkill", "myCareer"]
-  });
-});
+  loading.style.display = "none";
+  portfolio.style.display = "block";
+}, 3000);
